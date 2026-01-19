fcc9126d56f5cc7b0ed66b86b469779875ff31ab
Abigail Page <abbeyepage@gmail.com>
2026-01-19 10:10:36 +0000

    end of 18th changes

---
+++ foundationsofR/example script.R
@@ -0,0 +1,33 @@
+#===================================
+#PY1617: Foundations of R for Statistics and Data Science
+#Week 3: Working with data 
+#Author: Abigail Page & James Winters
+#Date: 16 January 2026
+#===================================
+
+#-----------------------------
+#Packages
+#-----------------------------
+library(tidyverse)
+
+#-----------------------------
+#Data import
+#-----------------------------
+
+iris_data <- iris (from a preinstalled one) 
+mydata <- read.csv(mydata) # from last week 
+
+
+#-----------------------------
+#Data exploration
+#-----------------------------
+
+# creates a top-level section
+## creates a subsection
+### creates a sub-subsection
+
+  
+  # Data exploration
+  ## Structure of the data
+  ### Summary statistics
+  
